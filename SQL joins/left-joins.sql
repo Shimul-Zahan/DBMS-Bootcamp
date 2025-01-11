eb0b246a36acb44dd30fdf83259dc4203da557ef
Xing Ping <112884750+Shimul-Zahan@users.noreply.github.com>
2025-01-11 23:26:31 +0600

    complete left  join

--- SQL joins/left-joins.sql
+++ SQL joins/left-joins.sql
@@ -1,30 +1,26 @@
--- Inner join / JOIN (same)
--- Ineer join use when the records are match
+-- Left join
+-- If are the record are same or not same
 -- Fetch the employee name and the department name they belong to
-
 select 
 	e.employee_name, d.department_name
 from 
 	public.employee e
-join 
+left join 
 	public.department d
 on 
-	-- join condition here
 	e.department_id = d.department_id
 
-
-
--- Fetch the employee name and the department name and manager name they belong to
+-- Fetch All the employee name and the department name and manager name they belong to
 
 select 
 	e.employee_id, e.employee_name, d.department_name, m.manager_name
 from 
 	public.employee e
-join 
+left join 
 	public.department d 
 on 
 	e.department_id = d.department_id
-join 
+left join 
 	public.manager m 
 on 
 	e.manager_id = m.manager_id
