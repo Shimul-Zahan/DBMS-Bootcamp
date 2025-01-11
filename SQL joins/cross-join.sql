8747a2600b872880d391e2fa22cce112f9fd0ba0
Xing Ping <112884750+Shimul-Zahan@users.noreply.github.com>
2025-01-12 00:15:54 +0600

    complete self join

--- SQL joins/cross-join.sql
+++ SQL joins/cross-join.sql
@@ -1,4 +1,17 @@
 -- A CROSS JOIN in SQL produces the Cartesian product of two tables
 -- match all records between two tables
--- 
+-- Cross join here
+-- total matches 28 ( dept 4 * employee 7)
+select 
+	e.employee_name, d.department_name, c.company_name, c.company_location
+from 
+	public.employee e
+left join 
+	public.department d
+on 
+	e.department_id = d.department_id
+cross join company c
 
+
+-- example here
+SQL joins\cross-join-table.png
